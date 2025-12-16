892cb6438efd38aafe62e49a4fdf13956229199a
0xhashcat <231815834+0xhashcatx0@users.noreply.github.com>
2025-12-16 18:34:52 +0100

    Colored

--- src/printer.rs
+++ src/printer.rs
@@ -1,16 +1,18 @@
+use colored::Colorize;
+
 pub fn print_header(msg: &str) {
-    println!("\x1b[34m### \x1b[0;1m{}\x1b[0m", msg);
+    println!("{}{}", "### ".blue(), msg.bold());
 }
 
 pub fn print_error(msg: &str, detail: Option<&str>) {
-    eprintln!("[\x1b[91m ERROR \x1b[0m] {}", msg);
+    eprintln!("{}", format!("ERROR {msg}").bright_red());
     if let Some(d) = detail {
         eprintln!("  -> {}", d);
     }
 }
 
 pub fn print_warning(msg: &str, detail: Option<&str>) {
-    eprintln!("[\x1b[93m WARN \x1b[0m] {}", msg);
+    eprintln!("{}", format!("ERROR {msg}").bright_yellow());
     if let Some(d) = detail {
         eprintln!("  -> {}", d);
     }
